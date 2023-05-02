4661f03b809ee41576da3f6ebb9651ea445a25dd
DJ <epicmrjones@gmail.com>
2023-05-02 10:29:47 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './Header.js';
+import Footer from './Footer.js';
+import Main from './Main.js';
+import './assets/css/app.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+    }
+  }
+
+  render() {
+    return(
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    )
+  }
+}
+
+export default App;
